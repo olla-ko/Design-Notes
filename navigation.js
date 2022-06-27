57e988b29b176bb19d109d88099729f4639ea80d
nkonovalov1990 <n.konovalov1990@gmail.com>
2022-06-27 22:41:06 +0400

    Меню работает

--- navigation.js
+++ navigation.js
@@ -1,27 +1,20 @@
 // $(".navigation").append("");
 
 {/* <ul>
-    <li>Вступление</li>
-    <li>Начинающему проектировщику</li>
-    <li>Общайся</li>
-    <li>Презентуй свои решения</li>
-    <li>Хочу все переделать</li>
-    <li>Мне достался сложный проект</li>
-    <li>Борем неуверенность</li>
-    <li>Красота vs удобство</li>
-    <li>Заблуждение: они обучатся</li>
-    <li>Заблуждение: мы потестим</li>
-    <li>Заблуждение: они настроят</li>
+    <li><a href="index.html">Вступление</a></li>
+    <li><a href="new-designer.html">Начинающему проектировщику</a></li>
+    <li><a href="talk.html">Общайся</a></li>
+    <li><a href="present.html">Презентуй свои решения</a></li>
+    <li><a href="redesign.html">Хочу все переделать</a></li>
+    <li><a href="too-hard.html">Мне достался сложный проект</a></li>
+    <li><a href="be-confident.html">Борем неуверенность</a></li>
+    <li><a href="beauty-vs-usability.html">Красота vs удобство</a></li>
+    <li><a href="they-will-learn.html">Заблуждение: они обучатся</a></li>
+    <li><a href="we-will-test.html">Заблуждение: мы потестим</a></li>
+    <li><a href="they-will-adjust.html">Заблуждение: они настроят</a></li>
 </ul> */}
 
-$(".navigation").append("<ul> <li>Вступление</li> <li>Начинающему проектировщику</li> <li>Общайся</li> <li>Презентуй свои решения</li> <li>Хочу все переделать</li> <li>Мне достался сложный проект</li> <li>Борем неуверенность</li> <li>Красота vs&nbsp;удобство</li> <li>Заблуждение: они обучатся</li> <li>Заблуждение: мы&nbsp;потестим</li> <li>Заблуждение: они настроят</li></ul>");
+$('.navigation').append('<ul> <li><a href="index.html">Вступление</a></li> <li><a href="new-designer.html">Начинающему проектировщику</a></li> <li><a href="talk.html">Общайся</a></li> <li><a href="present.html">Презентуй свои решения</a></li> <li><a href="redesign.html">Хочу все переделать</a></li> <li><a href="too-hard.html">Мне достался сложный проект</a></li> <li><a href="be-confident.html">Борем неуверенность</a></li> <li><a href="beauty-vs-usability.html">Красота vs&nbsp;удобство</a></li> <li><a href="they-will-learn.html">Заблуждение: они обучатся</a></li> <li><a href="we-will-test.html">Заблуждение: мы&nbsp;потестим</a></li> <li><a href="they-will-adjust.html">Заблуждение: они настроят</a></li></ul>');
 
-var header = $( "h1" ).text();
-
-// if ($("li").text() == header) {
-//     $("li").css('color', 'red');
-// }
-
-if (li.textContent.includes(header)) {
-    li.style.color = 'salmon';
-}
+var header = $('h1').text();
+$('a:contains('+ header +')').addClass('active');
