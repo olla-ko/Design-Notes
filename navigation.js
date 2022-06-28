508adc3b22b83828b3c2ae50fd928e63105f65f7
nkonovalov1990 <n.konovalov1990@gmail.com>
2022-06-28 08:06:35 +0400

    Сделал полоску у активного пункта меню

--- navigation.js
+++ navigation.js
@@ -17,4 +17,4 @@
 $('.navigation').append('<ul> <li><a href="index.html">Вступление</a></li> <li><a href="new-designer.html">Начинающему проектировщику</a></li> <li><a href="talk.html">Общайся</a></li> <li><a href="present.html">Презентуй свои решения</a></li> <li><a href="redesign.html">Хочу все переделать</a></li> <li><a href="too-hard.html">Мне достался сложный проект</a></li> <li><a href="be-confident.html">Борем неуверенность</a></li> <li><a href="beauty-vs-usability.html">Красота vs&nbsp;удобство</a></li> <li><a href="they-will-learn.html">Заблуждение: они обучатся</a></li> <li><a href="we-will-test.html">Заблуждение: мы&nbsp;потестим</a></li> <li><a href="they-will-adjust.html">Заблуждение: они настроят</a></li></ul>');
 
 var header = $('h1').text();
-$('a:contains('+ header +')').addClass('active');
+$('a:contains('+ header +')').parent('li').addClass('active');
