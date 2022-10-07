62db4719f5164e58decc9c3f4d32e1ea1c85f21b
nkonovalov1990 <n.konovalov1990@gmail.com>
2022-10-07 23:25:50 +0400

    1

--- navigation.js
+++ navigation.js
@@ -14,7 +14,7 @@
 
 // $(".navigation").append("");
 
-$('.navigation').append('<ul> <li><a href="index.html">Вступление</a></li> <li><a href="new-designer.html">Начинающему проектировщику</a></li> <li><a href="talk.html">Общайся</a></li> <li><a href="present.html">Презентуй свои решения</a></li> <li><a href="redesign.html">Хочу все переделать</a></li> <li><a href="too-hard.html">Мне достался сложный проект</a></li> <li><a href="be-confident.html">Борем неуверенность</a></li> <li><a href="beauty-vs-usability.html">Красота vs&nbsp;удобство</a></li> <li><a href="they-will-learn.html">Заблуждение: они&nbsp;обучатся</a></li> <li><a href="we-will-test.html">Заблуждение: мы&nbsp;потестим</a></li> <li><a href="they-will-adjust.html">Заблуждение: они&nbsp;настроят</a></li></ul>');
+$('.navigation').append('<ul> <li><a href="index.html">1. Вступление</a></li> <li><a href="new-designer.html">2. Начинающему проектировщику</a></li> <li><a href="talk.html">3. Общайся</a></li> <li><a href="present.html">4. Презентуй свои решения</a></li> <li><a href="redesign.html">5. Хочу все переделать</a></li> <li><a href="too-hard.html">6. Мне достался сложный проект</a></li> <li><a href="be-confident.html">7. Борем неуверенность</a></li> <li><a href="beauty-vs-usability.html">8. Красота vs&nbsp;удобство</a></li> <li><a href="they-will-learn.html">10. Заблуждение: они&nbsp;обучатся</a></li><li><a href="we-will-test.html">11. Заблуждение: мы&nbsp;потестим</a></li> <li><a href="they-will-adjust.html">12. Заблуждение: они&nbsp;настроят</a></li></ul>');
 
 var header = $('h1').text();
 $('a:contains('+ header +')').parent('li').addClass('active');
