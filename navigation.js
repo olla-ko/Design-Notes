3294a4059d6f937125a4e7c1302be41f070cfd67
nkonovalov1990 <n.konovalov1990@gmail.com>
2022-12-17 23:17:01 +0400

    Добавил предзагрузку всех страниц сайта
    В хроме должно работать быстрее

--- navigation.js
+++ navigation.js
@@ -2,6 +2,8 @@
 
 $('.navigation').append('<ul> <li><a href="index.html">Вступление</a></li> <li><a href="new-designer.html">1. Начинающему проектировщику</a></li> <li><a href="talk.html">2. Общайся</a></li> <li><a href="present.html">3. Презентуй свои решения</a></li> <li><a href="redesign.html">4. Хочу все переделать</a></li> <li><a href="too-hard.html">5. Мне достался сложный проект</a></li> <li><a href="be-confident.html">6. Борем неуверенность</a></li> <li><a href="beauty-vs-usability.html">7. Красота vs&nbsp;удобство</a></li> <li><a href="they-will-learn.html">8. Заблуждение: они&nbsp;обучатся</a></li><li><a href="we-will-test.html">9. Заблуждение: мы&nbsp;потестим</a></li> <li><a href="they-will-adjust.html">10. Заблуждение: они&nbsp;настроят</a></li> <li><a href="research.html">11. Измеряй</a></li></ul><div class="gradient-divider"></div><div class="donation"><img class="icon" src="IMG/wallet.svg"<span><a href="https://qiwi.com/n/OLLAKO" target="_blank">Скинуться на продление домена&nbsp;до&nbsp;2024 года</a></span></div><div class="gradient-divider"></div>');
 
+$('head').append('<link rel="prerender" href="be-confident.html"><link rel="prerender" href="beauty-vs-usability.html"><link rel="prerender" href="index.html"><link rel="prerender" href="new-designer.html"><link rel="prerender" href="present.html"><link rel="prerender" href="redesign.html"><link rel="prerender" href="research.html"><link rel="prerender" href="talk.html"><link rel="prerender" href="they-will-adjust.html"><link rel="prerender" href="they-will-learn.html"><link rel="prerender" href="too-hard.html"><link rel="prerender" href="we-will-test.html">');
+
 var header = $('h1').text();
 $('a:contains('+ header +')').parent('li').addClass('active');
 
